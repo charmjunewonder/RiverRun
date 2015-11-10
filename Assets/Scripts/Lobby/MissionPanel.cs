ac8224c4418cd9b42cfa9b908b2a795b3404f8b6
Eric <charmjunewonder@gmail.com>
2015-11-09 22:11:52 -0500

    Mission Complete UI

---
+++ Assets/Scripts/Lobby/MissionPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class MissionPanel : MonoBehaviour {
+
+    [SerializeField] Image userPanel;
+    [SerializeField] StarScript[] stars;
+    [SerializeField] Text RoleName;
+    [SerializeField] Text teamScore;
+    [SerializeField] Text rank;
+    [SerializeField] Slider randSlider;
+    [SerializeField] Text rankPercent;
+    [SerializeField] Text userName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Lobby/StarScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StarScript : MonoBehaviour {
+    [SerializeField]
+    Sprite[] stars;
+
+    public void ShowStar()
+    {
+        GetComponent<Image>().sprite = stars[1];
+    }
+
+    public void UnshowStar()
+    {
+        GetComponent<Image>().sprite = stars[0];
+    }
+}
