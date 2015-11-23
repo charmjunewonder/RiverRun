a8b9edc46fcfe17e9200bc08c2f67fe8554df8d7
EasternStar <feiranw@andrew.cmu.edu>
2015-11-23 09:14:39 -0500

    Server Camera Rotation

---
+++ Assets/Scripts/Environment/ServerCameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServerCameraMovement : MonoBehaviour {
+
+    private float angle;
+    private int flag;
+    private float radius;
+
+	void Start () {
+        flag = 1;
+	    angle = -90;
+        radius = Vector3.Distance(transform.position, new Vector3(0, transform.position.y, 0));
+	}
+	
+	void Update () {
+
+        angle += flag * Time.deltaTime;
+
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(radius * Mathf.Cos(angle * Mathf.PI / 180), pos.y, radius * Mathf.Sin(angle * Mathf.PI / 180));
+        transform.LookAt(new Vector3(0, 0, 0));
+
+        if (angle >= -75 || angle < -105) {
+            flag *= -1;
+        } 
+	}
+}
