1af2e3e0786ef21ebc8b40f0af426f27e28d6e91
Eric <charmjunewonder@gmail.com>
2015-11-22 22:41:02 -0500

    Tutorial Scene Button

---
+++ Assets/Scripts/Tutorial/TutorialController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void GoToToturialScene()
+    {
+        GameObject[] lobbies = GameObject.FindGameObjectsWithTag("LobbyDelete");
+        foreach(GameObject go in lobbies){
+            Destroy(go);
+        }
+        Application.LoadLevel("Tutorial");
+    }
+
+    public void GoToLobbyScene()
+    {
+        Application.LoadLevel("Lobby");
+    }
+}
