53706ee00f1113195bea178178ba535cff1fb338
Eric <charmjunewonder@gmail.com>
2015-11-29 16:41:02 -0500

    Story Scene

---
+++ Assets/Scripts/Utility/StoryController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class StoryController : MonoBehaviour {
+    public GameObject storyPanel;
+    public GameObject startPanel;
+
+	// Use this for initialization
+	void Start () {
+        startPanel.SetActive(true);
+        storyPanel.SetActive(false);
+#if UNITY_IOS
+        Invoke("GoToLobbyScene", 2);
+#endif
+#if UNITY_STANDALONE_WIN
+        Invoke("ShowStoryPanel", 2);
+#endif
+	}
+
+    void ShowStoryPanel()
+    {
+        startPanel.SetActive(false);
+        storyPanel.SetActive(true);
+        Invoke("GoToLobbyScene", 10);
+    }
+
+    public void GoToLobbyScene()
+    {
+        Application.LoadLevel("Lobby");
+    }
+}
