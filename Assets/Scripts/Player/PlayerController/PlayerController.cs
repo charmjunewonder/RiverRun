a0763576984202b573201ec407ae5a5fb90d369d
EasternStar <feiranw@andrew.cmu.edu>
2015-11-29 19:04:20 -0500

    crystal initialization is back

--- Assets/Scripts/Player/PlayerController/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController/PlayerController.cs
@@ -854,7 +854,7 @@ public class PlayerController : NetworkBehaviour {
     void OnLevelWasLoaded(int level)
     {
         Debug.Log("OnLevelWasLoaded");
-        if (level == 1)
+        if (level == 2)
         {
             initializeData();
 
@@ -863,7 +863,6 @@ public class PlayerController : NetworkBehaviour {
                 //if(level != 0)
                 //ClientScene.Ready(connectionToServer);
                 isInGame = true;
-                Debug.Log("Slot " + slot);
                 if (role == PlayerRole.Striker)
                     cam.cullingMask = (1 << (slot + 8)) | 1 | 1 << 13 | 1 << 12;
                 InitializeCrystal();
