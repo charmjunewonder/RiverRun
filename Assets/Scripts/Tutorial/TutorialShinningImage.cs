3633965c94952ce3e70ec41f617a0ef8de682831
EasternStar <feiranw@andrew.cmu.edu>
2015-12-06 00:33:25 -0500

    tutorial UI changes

---
+++ Assets/Scripts/Tutorial/TutorialShinningImage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialShinningImage : MonoBehaviour {
+
+    private float t;
+
+    void Start()
+    {
+        t = 0.5f;
+    }
+
+    void Update()
+    {
+        t -= Time.deltaTime;
+
+        if (t <= 0)
+        {
+            if (GetComponent<Image>().color.a == 0)
+                GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            else
+                GetComponent<Image>().color = new Color(1, 1, 1, 0);
+            t = 0.5f;
+        }
+    }
+}
